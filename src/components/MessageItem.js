8618472a77dbc45aee152b2e7d4628f47569c6a0
seagulls-lucky <seagulls.lucky125@gmail.com>
2021-08-08 08:50:37 +0900

    initial cards for add, delete

--- src/components/MessageItem.js
+++ src/components/MessageItem.js
@@ -1,6 +1,47 @@
+import React, { useEffect, useState } from 'react'
+import { createUseStyles } from 'react-jss'
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography';
+import {colors} from "@material-ui/core";
 
+
+const useStyle = createUseStyles({
+    root : {
+        margin : '1em',
+        minHeight : 100,
+    },
+    action : {
+        justifyContent : "flex-end"
+    }
+})
 
 const MessageItem = props => {
+
+    const { content, type, idx, removes } = props;
+    const backColors = {
+        1 : "#F56236",
+        2 : "#FCE788",
+        3 : "#88FCA3"
+    }
+    const classes = useStyle();
+
+    const removeItem = (e, idx) => {
+        removes(idx)
+    }
+
+    return (
+        <Card className={classes.root} style={{backgroundColor : backColors[type]}}>
+            <CardContent>
+                {content} + {idx}
+            </CardContent>
+            <CardActions className={classes.action}>
+                <Button size="small" onClick={(e)=>removeItem(e,idx)}>Clear</Button>
+            </CardActions>
+        </Card>
+    )
 
 }
 
--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,77 +1,152 @@
-import React, {Component, useEffect, useState, useRef} from 'react'
+import React, {useEffect, useState} from 'react'
 import Button from '@material-ui/core/Button'
 import Api from '../api'
 import {createUseStyles} from 'react-jss'
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
 
 import MessageItem from "./MessageItem";
 
 const useStyle = createUseStyles({
-
+    main: {
+        flexGrow: 1
+    },
+    buttons: {
+        textAlign: 'center'
+    }
 });
 
 const MessageList = props => {
-  const classes = useStyle();
-  const [messages, setMessages] = useState([]);
+    const classes = useStyle();
+    const [messages, setMessages] = useState([]);
 
-  const [errorList, setErrorList] = useState([]);
-  const [warningList, setWarningList] = useState([]);
-  const [infoList, setInfoList] = useState([]);
-  const [api] = useState(new Api({
-    messageCallback: (msg) => {
-      messageCallback(msg);
-    },
-  }))
+    const [errorList, setErrorList] = useState([]);
+    const [warningList, setWarningList] = useState([]);
+    const [infoList, setInfoList] = useState([]);
+    const [api] = useState(new Api({
+        messageCallback: (msg) => {
+            messageCallback(msg);
+        },
+    }))
 
-  useEffect(()=>{
-    api.start();
-  },[])
+    useEffect(() => {
+        api.start();
+    }, [])
 
-  function messageCallback(msg) {
-    messages.push(msg);
-    setMessages([...messages]);
-    console.log(messages);
-    switch (msg.priority) {
-      case 1 :
-        errorList.push(msg);
-        setErrorList([...errorList]);
-        break;
-      case 2 :
-        warningList.push(msg);
-        setWarningList([...warningList]);
-        break;
-      case 3 :
-        infoList.push(msg);
-        setInfoList([...infoList]);
-        break;
+    const messageCallback = msg => {
+        // const new_message = Object.assign(messages, []);
+        // new_message.push(msg)
+        // setMessages(new_message);
+        switch (msg.priority) {
+            case 1 :
+                setErrorList(errorList => {
+                    errorList.push(msg);
+                    return [...errorList]
+                });
+                break;
+            case 2 :
+                setWarningList(warningList => {
+                    warningList.push(msg);
+                    return [...warningList]
+                });
+                break;
+            case 3 :
+                setInfoList(infoList => {
+                    infoList.push(msg);
+                    return [...infoList]
+                });
+                break;
+        }
     }
-  }
 
-  const renderButton = () => {
-    const isApiStarted = api.isStarted()
+    const removeAll = () => {
+        setWarningList([]);
+        setErrorList([]);
+        setInfoList([]);
+    }
+
+    const renderButton = () => {
+        const isApiStarted = api.isStarted()
+        return (
+            <>
+                <Button
+                    variant="contained"
+                    onClick={() => {
+                        if (isApiStarted) {
+                            api.stop()
+                        } else {
+                            api.start()
+                        }
+                        //force update
+                        setMessages([...messages])
+                    }}
+                    style={{backgroundColor: "#88FCA3"}}
+                >
+                    {isApiStarted ? 'Stop' : 'Start'}
+                </Button>
+                <Button style={{backgroundColor: "#88FCA3", marginLeft: 20}} variant="contained" onClick={removeAll}>
+                    Clear
+                </Button>
+            </>
+
+        )
+    }
+
+    const handleRemove = (idx, type) => {
+        switch (type) {
+            case 1 :
+                errorList.splice(idx, 1);
+                setErrorList([...errorList]);
+                break;
+            case 2 :
+                warningList.splice(idx, 1);
+                setWarningList([...warningList]);
+                break;
+            case 3 :
+                infoList.splice(idx, 1);
+                setInfoList([...infoList]);
+                break;
+        }
+        setMessages([...messages])
+    }
+
     return (
-      <Button
-        variant="contained"
-        onClick={() => {
-          if (isApiStarted) {
-            api.stop()
-          } else {
-            api.start()
-          }
-          //force update
-          setMessages([...messages])
-        }}
-      >
-        {isApiStarted ? 'Stop Messages' : 'Start Messages'}
-      </Button>
+        <React.Fragment>
+            <Container fixed maxWidth="lg">
+                <div className={classes.buttons}>
+                    {renderButton()}
+                </div>
+                <div className={classes.main}>
+                    <Grid container spacing={3}>
+                        <Grid item xs={12} lg={4}>
+                            {
+                                errorList.map((list, index) => (
+                                    <MessageItem content={list.message} type={list.priority} key={index} idx={index}
+                                                 removes={(idx) => handleRemove(idx, 1)}/>
+                                ))
+                            }
+                        </Grid>
+                        <Grid item xs={12} lg={4}>
+                            {
+                                warningList.map((list, index) => (
+                                    <MessageItem content={list.message} type={list.priority} key={index} idx={index}
+                                                 removes={(idx) => handleRemove(idx, 2)}/>
+                                ))
+                            }
+                        </Grid>
+                        <Grid item xs={12} lg={4}>
+                            {
+                                infoList.map((list, index) => (
+                                    <MessageItem content={list.message} type={list.priority} key={index} idx={index}
+                                                 removes={(idx) => handleRemove(idx, 3)}/>
+                                ))
+                            }
+                        </Grid>
+                    </Grid>
+                </div>
+            </Container>
+        </React.Fragment>
     )
-  }
-
-
-  return (
-    <div>
-      {renderButton()}
-    </div>
-  )
 
 }
 
